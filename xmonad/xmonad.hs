c97977f816106534852a62375140b81353e2bf38
Clinton R. Nixon <crnixon@gmail.com>
2010-01-06 15:18:26 -0500

    updated xmonad conf

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,11 +1,46 @@
 import XMonad
 import XMonad.Config.Gnome
-import XMonad.Util.EZConfig
-	
-main = xmonad $ gnomeConfig {
-         terminal = "gnome-terminal"
-       , modMask = mod4Mask
-       }
-       `additionalKeysP`
-       [ ("M-d", spawn "dmenu_run -b") ]
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.ManageHook
+import XMonad.Hooks.ManageHelpers
+import XMonad.Layout.WindowNavigation
+import qualified Data.Map as M
+ 
+-- Actions
+import XMonad.Actions.CycleWS
+import XMonad.Actions.SwapWorkspaces
 
+isSplash = isInProperty "_NET_WM_WINDOW_TYPE" "_NET_WM_WINDOW_TYPE_SPLASH"
+
+myManageHook :: [ManageHook]
+myManageHook = 
+    [ isSplash --> doIgnore ]
+
+main = do xmonad $ gnomeConfig
+                     { manageHook = manageHook gnomeConfig <+> composeAll myManageHook,
+                       layoutHook = windowNavigation $ layoutHook gnomeConfig,
+                       keys = myKeys,
+                       borderWidth = 3,
+                       focusedBorderColor = "#FFF838",
+                       normalBorderColor = "#444447",
+                       modMask = mod4Mask }
+          where
+            defKeys = keys defaultConfig
+            delKeys x = foldr M.delete (defKeys x) (toRemove x)
+            myKeys x = foldr (uncurry M.insert) (delKeys x) (toAdd x)
+ 
+          -- remove some of the default key bindings
+            toRemove x =
+                [ (modMask x              , xK_p  )
+                , (modMask x .|. shiftMask, xK_q  ) -- don't strand naive users
+                ]
+ 
+            toAdd x =
+                [ ((modMask x, xK_Right), sendMessage $ Go R)
+		, ((modMask x, xK_Left ), sendMessage $ Go L)
+		, ((modMask x, xK_Up   ), sendMessage $ Go U)
+		, ((modMask x, xK_Down ), sendMessage $ Go D)
+                ]
+            
+     
