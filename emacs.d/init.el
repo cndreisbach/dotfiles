b55289019da99e9745c35bbbffaced5681a6a18e
Clinton R. Nixon <crnixon@gmail.com>
2011-09-30 15:51:36 -0400

    Emacsclient improvements

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -66,6 +66,13 @@
 ;;; Local code
 (load "functions.el")
 
+;;; Server setup
+(add-hook 'server-switch-hook
+            (lambda ()
+              (when (current-local-map)
+                (use-local-map (copy-keymap (current-local-map))))
+	      (when server-buffer-clients
+		(local-set-key (kbd "C-x k") 'server-edit))))
 
 ;;; Keybindings
 (global-set-key (kbd "RET") 'newline-and-indent)
