c0c71db319ed7456032085a5e6538da95f18133c
Clinton N. Dreisbach <crnixon@gmail.com>
2013-06-10 10:04:37 -0400

    Add lein/profiles.clj

---
+++ lein-profiles.clj
@@ -0,0 +1,9 @@
+{:user {:plugins [[lein-catnip "0.5.1"]
+                  [lein-kibit "0.0.8"]
+                  [jonase/eastwood "0.0.2"]
+                  [lein-outdated "1.0.0"]
+                  [lein-marginalia "0.7.1"]
+                  [lein-grep "0.1.0"]
+                  [lein-bikeshed "0.1.0"]]
+        :dependencies [[alembic "0.1.3"]
+                       [org.clojure/tools.namespace "0.2.3"]]}}
