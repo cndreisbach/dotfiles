7e5d671361875936fabb42e14da25d9517654ee5
Clinton N. Dreisbach <crnixon@gmail.com>
2013-10-27 14:54:59 -0400

    Added @spencertipping's cd

---
+++ .bash/cd.sh
@@ -0,0 +1,10 @@
+. ~/dotfiles/lib/cd/cd                      # enables extensible cd
+. ~/dotfiles/lib/cd/cd-traverse             # enables descendant/ancestor traversal
+. ~/dotfiles/lib/cd/cd-history              # ^, ^^, ^n, ^regexp
+. ~/dotfiles/lib/cd/cd-ssh                  # does nothing unless you have sshfs
+. ~/dotfiles/lib/cd/cd-archive              # does nothing unless you have archivemount
+#. ~/dotfiles/lib/cd/cd-hdfs                 # does nothing unless you have hadoop-fuse-dfs
+#. ~/dotfiles/lib/cd/cd-git                  # does nothing unless you have yagfs
+. ~/dotfiles/lib/cd/cd-encfs                # does nothing unless you have encfs
+. ~/dotfiles/lib/cd/cd-dev                  # enables automounting for /dev/ entries
+#. ~/dotfiles/lib/cd/cd-missing-mkdir        # enables auto-mkdir
--- setup.sh
+++ setup.sh
@@ -4,3 +4,5 @@ if [ ! -d ~/.vim/bundle/vundle ]; then
   echo "Installing Vundle"
   git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 fi
+
+cd ~/dotfiles && git submodule update --init --quiet
