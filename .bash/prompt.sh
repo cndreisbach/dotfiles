2975ff0d7d6c3f56a94b03bb144c34b935d27e2d
Clinton N. Dreisbach <crnixon@gmail.com>
2013-10-27 21:23:11 -0400

    Changed prompt to not make export print colors

--- .bash/prompt.sh
+++ .bash/prompt.sh
@@ -1,23 +1,20 @@
-# @gf3’s Sexy Bash Prompt, inspired by “Extravagant Zsh Prompt”
-# Shamelessly copied from https://github.com/gf3/dotfiles
-# Screenshot: http://i.imgur.com/s0Blh.png
+# My bash prompt, heavily inspired by @gf3’s Sexy Bash Prompt
 
 if [[ $COLORTERM = gnome-* && $TERM = xterm ]] && infocmp gnome-256color >/dev/null 2>&1; then
-	export TERM=gnome-256color
+  export TERM=gnome-256color
 elif infocmp xterm-256color >/dev/null 2>&1; then
-	export TERM=xterm-256color
+  export TERM=xterm-256color
 fi
 
-
 if tput setaf 1 &> /dev/null; then
-	tput sgr0
-  DIRC=$(tput setaf 6)
-  GITC=$(tput setaf 7)
-  PROMPTC=$(tput setaf 14)
-  ERRORC=$(tput setaf 1)
-  TIMERC=$(tput setaf 9)
-	BOLD=$(tput bold)
-	RESET=$(tput sgr0)
+  tput sgr0
+  DIRC="tput setaf 6"
+  GITC="tput setaf 7"
+  PROMPTC="tput setaf 6"
+  ERRORC="tput setaf 1"
+  TIMERC="tput setaf 9"
+  BOLD="tput bold"
+  RESET="tput sgr0"
 fi
 
 export DIRC
@@ -29,12 +26,12 @@ export BOLD
 export RESET
 
 function parse_git_dirty() {
-	[[ $(git status 2> /dev/null | tail -n1) != *"nothing to commit"* ]] && echo "*"
+  [[ $(git status 2> /dev/null | tail -n1) != *"nothing to commit"* ]] && echo "*"
 }
 
 function parse_git_branch() {
-  GIT_BRANCH=$(git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/\1$(parse_git_dirty)/")
-  [[ $GIT_BRANCH != "" ]] && echo " $GIT_BRANCH"
+  local git_branch=$(git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/\1$(parse_git_dirty)/")
+  [[ $git_branch != "" ]] && echo " $git_branch"
 }
 
 function timer_start() {
@@ -47,13 +44,16 @@ function timer_stop() {
 }
 
 function prompt_command() {
-  exit_status=$?
+  local exit_status=$?
   timer_stop
-  time_display=""
+
+  local time_display=""
+  local prompt_color=$($PROMPTC)
+
   [[ $timer_show -ge "5" ]] && time_display=" ${timer_show}s"
-  color=$PROMPTC
-  [[ $exit_status -ne "0" ]] && color="$ERRORC$BOLD"
-  PS1="\n${DIRC}\w${GITC}\$(parse_git_branch)${TIMERC}${time_display} ${RESET}[rb: \$(rbenv version-name)]\n${color}\$${RESET} "
+  [[ $exit_status -ne "0" ]] && prompt_color="$($ERRORC)$($BOLD)"
+
+  PS1="\n$($DIRC)\w$($GITC)\$(parse_git_branch)$($TIMERC)${time_display}$($RESET)\n${prompt_color}\$$($RESET) "
 }
 
 trap 'timer_start' DEBUG
