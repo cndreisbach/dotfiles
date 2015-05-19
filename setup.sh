278eeba2135370a05608065539d6a59583e99c1c
Clinton Dreisbach <clinton@dreisbach.us>
2015-05-18 21:39:41 -0400

    Latest updates

--- setup.sh
+++ setup.sh
@@ -1,12 +1,3 @@
 #!/bin/sh
 
 cd ~/dotfiles && git submodule update --init --quiet
-
-if [ ! -d ~/.vim/bundle/vundle ]; then
-  echo "Installing Vundle"
-  git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-fi
-
-if [ ! -e ~/bin/gherkin ]; then
-  ln -s ~/dotfiles/lib/gherkin/gherkin ~/bin/gherkin
-fi
