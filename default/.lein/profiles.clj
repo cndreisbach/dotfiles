7c4040b33c9124014ff2614b8a65b4d511d83ba3
Clinton N. Dreisbach <crnixon@gmail.com>
2015-09-14 19:53:53 -0400

    update

--- default/.lein/profiles.clj
+++ default/.lein/profiles.clj
@@ -1,11 +1,11 @@
 
-{:user {:plugins [[lein-ancient "0.5.5"]
+{:user {:plugins [[lein-ancient "0.6.7"]
                   [lein-marginalia "0.8.0"]
-                  [lein-grep "0.1.0"]
+                  [lein-grep "0.1.1"]
                   [lein-try "0.4.3"]
-                  [lein-gorilla "0.3.3"]]
-        :dependencies [[alembic "0.2.1"]
+                  [lein-gorilla "0.3.4"]]
+        :dependencies [[alembic "0.3.2"]
                        [criterium "0.4.3"]
-                       [org.clojure/tools.namespace "0.2.6"]
+                       [org.clojure/tools.namespace "0.2.10"]
                        [slamhound "1.5.5"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
