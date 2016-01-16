05d71b5f992723ab5dbe9765b23034b2916356cd
Clinton N. Dreisbach <crnixon@gmail.com>
2016-01-16 13:49:48 -0500

    Add /usr/local/sbin to PATH

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -15,7 +15,9 @@ end
 
 eval (direnv hook fish)
 
-source ~/.config/fish/conda.fish
+if test -f ~/.config/fish/conda.fish
+  source ~/.config/fish/conda.fish
+end
 
 if test -f ~/.config/fish/local.fish
   source ~/.config/fish/local.fish
