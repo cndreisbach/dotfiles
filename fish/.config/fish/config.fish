a2c0decc6ba18d55d7859dec5d59f8484035cb92
Clinton N. Dreisbach <clinton@zomp.local>
2016-02-27 16:50:20 -0500

    Remove fisherman

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,16 +1,7 @@
-### Fisherman
-set fisher_home ~/vendor/fisherman
-set fisher_config ~/.config/fisherman
-source $fisher_home/config.fish
-
 # Utilities
 
 function -e fish_preexec _run_fasd
   fasd --proc (fasd --sanitize "$argv") > "/dev/null" 2>&1
-end
-
-function z
-  cd (fasd -d -e 'printf %s' "$argv")
 end
 
 eval (direnv hook fish)
