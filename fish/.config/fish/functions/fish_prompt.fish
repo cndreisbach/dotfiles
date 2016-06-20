6990e03a77f4a168696c54cbb303a54912e4954c
Clinton Dreisbach <cdreisbach@rti.org>
2016-06-20 09:50:01 -0400

    changes from rti-100623.rtp.rti.org on Mon Jun 20 09:50:01 EDT 2016

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -89,7 +89,7 @@ function fish_prompt --description 'Write out the prompt'
 
     set -l __conda_env $CONDA_DEFAULT_ENV
     if test -n $__conda_env
-        set -l __conda_env "($__conda_env) "
+        set __conda_env "($__conda_env) "
     end
 
 	echo -n -s "$__conda_env" (set_color $fish_color_user) "$USER" $normal @ (set_color $fish_color_host) "$__fish_prompt_hostname" $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal (__fish_git_prompt) $normal $prompt_status "$mode_str" "> "
