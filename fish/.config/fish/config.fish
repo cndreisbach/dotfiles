060226413b31264c0330f601d215384bcafdc04a
Clinton Dreisbach <cdreisbach@rti.org>
2017-02-24 10:44:51 -0500

    Fix ssh-agent on fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -10,3 +10,9 @@ if test -f ~/.config/fish/local.fish
   source ~/.config/fish/local.fish
 end
 
+# Fundle
+if not functions -q fundle; eval (curl -sfL https://git.io/fundle-install); end
+
+fundle plugin 'tuvistavie/fish-ssh-agent'
+
+fundle init
